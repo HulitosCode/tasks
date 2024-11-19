4a8c2a8adcfa35b99512b3b89c0a7eef5405a11b
HulitosCode <heltonmatimbe8@gmail.com>
2024-11-19 15:38:10 +0200

    feat: adicionando shadcn

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+    return ( 
+        <div className="px-10 py-10 items-center flex justify-between">
+            <Button className="w-20">Home</Button>
+            <Button className="w-20">Home</Button>
+        </div>
+     );
+}
+ 
+export default HomePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Mulish } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const mulish = Mulish({
+  subsets: ["latin-ext"],
 });
 
 export const metadata: Metadata = {
@@ -25,10 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${mulish.className} dark antialiased`}>
+          <div className="flex h-full flex-col overflow-hidden">{children}</div>
       </body>
     </html>
   );
